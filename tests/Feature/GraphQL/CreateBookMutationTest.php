8acf1aefd1e50c5e37412522ea331a40e064ac5d
Sarah Ting (macbook) <sarah.j.ting@gmail.com>
2020-04-27 20:22:11 +0900

    add validation testing for create book mutation

--- tests/Feature/GraphQL/CreateBookMutationTest.php
+++ tests/Feature/GraphQL/CreateBookMutationTest.php
@@ -13,15 +13,47 @@ class CreateBookMutationTest extends TestCase
 {
     use RefreshDatabase, WithFaker, TestsGraphQL;
 
+    private function createBookMutationSignature() {
+        return 'mutation createBook($book: BookInput) { createBook(book: $book){ title author{name} } }';
+    }
+
     public function test_can_create_book()
     {
         $rawBook = Arr::only(factory(\App\Models\Book::class)->states("raw")->raw(), ["title", "author"]);
         
         $response = $this->callGraphQL(
-            'mutation createBook($book: BookInput) { createBook(book: $book){ title author{name} } }',
+            $this->createBookMutationSignature(),
             [ "book" => $rawBook ],
         );
 
         $this->assertGraphQLFragment($response, ["createBook" => $rawBook]);
+    }
+
+    public function test_cannot_create_book_without_title()
+    {
+        $rawBook = Arr::only(factory(\App\Models\Book::class)->states("raw")->raw([
+            "title" => ""
+        ]), ["title", "author"]);
+        
+        $response = $this->callGraphQL(
+            $this->createBookMutationSignature(),
+            [ "book" => $rawBook ],
+        );
+
+        $response->assertJsonStructure(["errors"]);
+    }
+
+    public function test_cannot_create_book_without_author()
+    {
+        $rawBook = Arr::only(factory(\App\Models\Book::class)->states("raw")->raw([
+            "author" => ["name" => ""]
+        ]), ["title", "author"]);
+        
+        $response = $this->callGraphQL(
+            $this->createBookMutationSignature(),
+            [ "book" => $rawBook ],
+        );
+
+        $response->assertJsonStructure(["errors"]);
     }
 }
