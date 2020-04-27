cf42ad18c150a08fe4f7d9a48b38a3f010ec61a1
Sarah Ting (macbook) <sarah.j.ting@gmail.com>
2020-04-27 16:07:14 +0900

    install graphql package

--- config/app.php
+++ config/app.php
@@ -165,6 +165,7 @@ return [
         /*
          * Package Service Providers...
          */
+        Rebing\GraphQL\GraphQLServiceProvider::class,
 
         /*
          * Application Service Providers...
@@ -226,6 +227,8 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
+        
+        'GraphQL' => Rebing\GraphQL\Support\Facades\GraphQL::class,
 
     ],
 
