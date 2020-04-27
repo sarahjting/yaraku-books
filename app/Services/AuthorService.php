2c2bd4efb7d09a5efc12317e4006142f8ae61189
Sarah Ting (macbook) <sarah.j.ting@gmail.com>
2020-04-27 20:19:15 +0900

    tidy author service

--- app/Services/AuthorService.php
+++ app/Services/AuthorService.php
@@ -4,26 +4,28 @@ namespace App\Services;
 
 use App\Models\Author;
 
+use Illuminate\Database\Eloquent\Collection;
+
 class AuthorService {
 
-    public function create($data) {
+    public function create(array $data): Author {
         return Author::create($this->sanitizeInput($data));
     }
 
-    public function firstOrCreate($data) {
+    public function firstOrCreate(array $data): Author {
         $author = $this->firstWithName($data["name"]);
         if($author === null) $author = $this->create($data);
         return $author;
     }
 
-    public function firstWithName($name) {
+    public function firstWithName(string $name):? Author {
         $data = $this->sanitizeInput(["name" => $name]);
         return Author::where("given_name", $data['given_name'])
             ->where("family_name", $data['family_name'])
             ->first();
     }
 
-    public function getByName($name) {
+    public function getByName(string $name):? Collection {
         if(strpos($name, " ") === false) {
             return Author::where(function($query) use($name) {
                 $query->where("given_name", "LIKE", "{$name}%")
@@ -37,7 +39,7 @@ class AuthorService {
         }
     }
 
-    public function sanitizeInput($input) {
+    public function sanitizeInput(array $input): array {
         return [
             'given_name' => $input['given_name'] ?? implode(" ", array_slice(explode(" ", $input['name']), 0, -1)),
             'family_name' => $input['family_name'] ?? array_slice(explode(" ", $input["name"]), -1)[0],
