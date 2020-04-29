a35a039305582d848aa08b101b818dd483cca581
Sarah Ting (macbook) <sarah.j.ting@gmail.com>
2020-04-29 15:40:33 +0900

    add extra tests for exporting books in csv

--- tests/Feature/ExportBooksTest.php
+++ tests/Feature/ExportBooksTest.php
@@ -4,14 +4,16 @@ namespace Tests\Feature;
 
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\Traits\TestsXML;
+use Tests\Traits\TestsCSV;
 
+use App\Models\Collections\CSVFormattableCollection;
 use App\Models\Collections\XMLFormattableCollection;
 
 use Tests\TestCase;
 
 class ExportBooksTest extends TestCase
 {
-    use RefreshDatabase, TestsXML;
+    use RefreshDatabase, TestsXML, TestsCSV;
 
     public function test_can_export_empty_xml()
     {
@@ -28,5 +30,27 @@ class ExportBooksTest extends TestCase
 
         $response->assertStatus(200);
         $this->assertXMLEquals($books->sortBy("title")->toXML("books")->asXML(), $response->getContent());
+    }
+
+    public function test_can_export_xml_with_custom_fields()
+    {
+        $books = new XMLFormattableCollection(factory(\App\Models\Book::class, 10)->create());
+        $response = $this->get("/export/books?fields=title,author.name");
+
+        $response->assertStatus(200);
+        $this->assertXMLEquals($books->sortBy("title")->toXML("books", ["title", "author.name"])->asXML(), $response->getContent());
+    }
+
+    public function test_can_export_csv()
+    {
+        factory(\App\Models\Book::class, 10)->create();
+        $response = $this->get("/export/books?fields=id,title,author.name", [
+            "accept" => "text/csv"
+        ]);
+
+        $response->assertStatus(200);
+
+        $books = new CSVFormattableCollection(\App\Models\Book::orderBy('title')->get());
+        $this->assertCSVEquals($books->toCSV(["id", "title", "author.name"]), $response->getContent());
     }
 }
