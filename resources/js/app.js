8c774a8b4031ad95e3c176729cbefc23ececd021
Sarah Ting (macbook) <sarah.j.ting@gmail.com>
2020-04-29 18:31:58 +0900

    install vuetify

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,7 +1,9 @@
 import "./bootstrap";
 import Vue from "vue";
 import App from "./App.vue";
+import vuetify from "./plugins/vuetify";
 
 new Vue({
     render: (h) => h(App),
+    vuetify,
 }).$mount("#app");
