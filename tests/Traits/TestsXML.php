c93dbdb2d7471941363f665c3c205cfc33847118
Sarah Ting (macbook) <sarah.j.ting@gmail.com>
2020-04-29 15:15:45 +0900

    tidy whitespace

--- tests/Traits/TestsXML.php
+++ tests/Traits/TestsXML.php
@@ -7,6 +7,7 @@ trait TestsXML
     protected function assertXMLEquals(string $expectedXML, string $actualXML) {
         $this->assertEquals($this->filterXml($actualXML), $this->filterXml($expectedXML));
     }
+    
     protected function filterXML($xml) {
         return str_replace(["\n", "<?xml version=\"1.0\"?>"], "", $xml);
     }
