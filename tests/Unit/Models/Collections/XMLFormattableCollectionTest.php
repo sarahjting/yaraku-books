4518751fa015e8e1e0f8c855ab8385411a4a79f3
Sarah Ting (macbook) <sarah.j.ting@gmail.com>
2020-04-29 14:21:20 +0900

    rename generic formatter class

--- tests/Unit/Models/Collections/XMLFormattableCollectionTest.php
+++ tests/Unit/Models/Collections/XMLFormattableCollectionTest.php
@@ -11,7 +11,7 @@ use Illuminate\Foundation\Testing\RefreshDatabase;
 use Arr;
 use SimpleXMLElement;
 
-class FormattableCollectionTest extends TestCase
+class XMLFormattableCollectionTest extends TestCase
 {
     use RefreshDatabase, TestsXML;
 
