fd8b4895889e5692c3bfec8e432519de0af7b586
Sarah Ting (macbook) <sarah.j.ting@gmail.com>
2020-04-30 15:02:42 +0900

    implement createBook mutation into api object

--- tests/Client/API/index.spec.js
+++ tests/Client/API/index.spec.js
@@ -1,14 +1,20 @@
-import axios from "axios";
-
-import httpAdapter from "axios/lib/adapters/http";
-import https from "https";
-
 import api from "../../../resources/js/api/index";
 
 describe("API", () => {
     // graphql is tested by PHPUnit, so i only want to test that the endpoints are returning 200 and no error messages
     it("should get a list of books from GraphQL", async () => {
-        let books = await api.books();
+        const books = await api.books();
         expect(Array.isArray(books)).toBeTruthy();
+    });
+    it("should create a book via GraphQL", async () => {
+        const rawBook = {
+            title: "Foo Bar",
+            author: { name: "Foodinand Bartholomew" }
+        };
+        let book = await api.createBook(rawBook);
+
+        expect(typeof book).toBe("object");
+        expect(book.title).toBe(rawBook.title);
+        expect(book.author.name).toBe(rawBook.author.name);
     });
 });
