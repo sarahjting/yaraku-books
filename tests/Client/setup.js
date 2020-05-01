2a6b94875ceb1bff4d3ce5d56602fa4a1eb2cab5
Sarah Ting (macbook) <sarah.j.ting@gmail.com>
2020-05-02 01:33:32 +0900

    add workaround to stop vuetify complaints in frontend tests

--- tests/Client/setup.js
+++ tests/Client/setup.js
@@ -7,3 +7,9 @@ require("dotenv").config();
 
 Vue.use(vuetify);
 Vue.use(Vuex);
+
+// https://github.com/vuetifyjs/vuetify/issues/3456#issuecomment-408424406
+// required to stop vuetify from complaining about not being able to find [data-app]
+const el = document.createElement("div");
+el.setAttribute("data-app", true);
+document.body.appendChild(el);
